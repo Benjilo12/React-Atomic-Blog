cd5ddf1a04c334ea076f1b4493b1c1869fc7e8cc
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2024-05-18 07:42:36 +0000

    first commit

---
+++ src/PostContext.js
@@ -0,0 +1,67 @@
+import { Children, createContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+//!Creating a custom Provider hook/ exporting the provider hook
+
+//this becomes the children of this component
+{
+  /* <PostPrivider>
+<Header />
+<Main />
+<Archive />
+<Footer />
+</PostPrivider>
+</section> */
+}
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+//using usecontext
+// 1)Create a provider
+const PostContext = createContext();
+
+function PostPrivider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+  return (
+    //2) Provide value to the child component
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+export { PostPrivider, PostContext };
